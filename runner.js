fc3eee522a514421f431d5e83575d7c1099c55a4
Kamil <kamil.marusia@upsanok.onmicrosoft.com>
2026-03-27 20:54:00 +0100

    porownywania

--- runner.js
+++ runner.js
@@ -2,6 +2,24 @@ const age = 50;
 let myAge = 99;
 //age = 101; próba nadania nowej wartości zmiennej age zadeklarowanej za pomocą const.
 const myName = "Kamil";
+
+const human = {
+    age: 23,
+    name: "Kamil",
+};
+
+const human2 = {
+    age: 20,
+    name: "Antoni",
+};
+
+const human3 = human2;
+console.log(human);
+console.log(human2)
+
+console.log("human2 ==== human3", human2 === human3);
+console.log("human ==== human2", human === human2);
+
 console.log(myAge);
 console.log(age);
 console.log("Moje imie to", myName);
