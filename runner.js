b967bc85966f0f5be58991a0f8590d05bba6b430
Kamil <kamil.marusia@upsanok.onmicrosoft.com>
2026-03-27 20:38:11 +0100

    forget

---
+++ runner.js
@@ -0,0 +1,16 @@
+const age = 50;
+let myAge = 99;
+//age = 101; próba nadania nowej wartości zmiennej age zadeklarowanej za pomocą const.
+const myName = "Kamil";
+console.log(myAge);
+console.log(age);
+console.log("Moje imie to", myName);
+
+
+
+
+
+
+
+
+
